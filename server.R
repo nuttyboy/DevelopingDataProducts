65293fdc39276ef2dc7d96b7f5c4cc349c16e11e
Nutty Boy <natarajan.mani@outlook.com>
2020-05-24 02:28:10 +1000

    Add files via upload
    Commit 1

---
+++ server.R
@@ -0,0 +1,67 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(ggplot2)
+data(swiss)
+
+shinyServer(function(input, output) {
+  #Model 1
+  model1 <-  lm(Fertility~Agriculture,data=swiss)
+  #Model 2
+  model2 <-  lm(Fertility~Catholic,data=swiss)
+  #Model 3
+  model3 <-  lm(Fertility~Infant.Mortality,data=swiss)
+
+#RMSE  
+  
+  modelpred <- reactive({
+    
+    if(input$Indi=="Agriculture") {
+    RMSE(predict(model1,swiss),swiss$Fertility)
+    }
+    else
+    if(input$Indi=="Catholic") {
+      RMSE(predict(model2,swiss),swiss$Fertility)
+    }
+    
+    else
+      if(input$Indi=="Infant.Mortality") {
+        RMSE(predict(model3,swiss),swiss$Fertility)
+      }
+  
+    })
+  
+#Plot outputs
+  output$plot <- renderPlot({
+    if(input$Indi=="Agriculture") {
+    plot(swiss$Agriculture,swiss$Fertility, xlab = "Agriculture(%)",ylab = "Fertility(lg)", bty = "n", pch = 16,main="Fertility ~ Agriculture")
+    abline(model1,col="red")
+    }
+    else
+      if(input$Indi=="Catholic") {
+        plot(swiss$Catholic,swiss$Fertility, xlab = "Catholic(%)",ylab = "Fertility(lg)", bty = "n", pch = 16,main="Fertility ~ Catholic")
+        abline(model2,col="blue")
+      }
+    else
+      if(input$Indi=="Infant.Mortality") {
+        plot(swiss$Infant.Mortality,swiss$Fertility, xlab = "Infant.Mortality(%)",ylab = "Fertility(lg)", bty = "n", pch = 16,main="Fertility ~ Infant.Mortality")
+        abline(model3,col="green")
+      }
+    
+    
+  })
+  
+  output$pred1 <- renderText({
+    modelpred()
+  })
+  
+  
+  
+  })
---
+++ ui.R
@@ -0,0 +1,34 @@
+
+
+library(shiny)
+
+
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Which is the Good Preictor of Fertility"),
+  
+
+  sidebarLayout(
+        sidebarPanel(
+            helpText("Have a look at swiss data using '?swiss' on Rstudio Console"),
+            helpText("Here we are looking for the best predictor for Fertility using the linear regression model"),
+            helpText("Below you can choose a predictor to see the respective Indicator plot and Root Mean Squeared error"),
+            radioButtons(inputId="Indi",
+                       label = "Indicators",
+                     choices = c("Agriculture","Catholic","Infant.Mortality"),
+                     inline = FALSE
+        )
+        
+        
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("plot"),
+      h3("The RMSE is:"),
+      textOutput("pred1")
+      
+    )
+  )
+))
